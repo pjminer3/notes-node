cd2e87064f3459ea5111ece7dcb187d8450f098f
Patrick Miner <patrickminer318@gmail.com>
2017-10-03 20:33:19 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+
+
+
+let user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!  You are ${notes.age} years old! \n`, function(err) {
+  if (err) {
+    console.log('Unable to write file');
+  }
+});
