7bd76e23d2c629bddd82f8749324aae3a9107018
Patrick Miner <patrickminer318@gmail.com>
2017-10-04 15:11:32 -0700

    Formatted app.js & notes.js for further development of actual app

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
 
-let user = os.userInfo();
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!  You are ${notes.age} years old! \n`, function(err) {
-  if (err) {
-    console.log('Unable to write file');
-  }
-});
+
--- notes.js
+++ notes.js
@@ -2,13 +2,7 @@ console.log('Starting notes.js');
 
 //modue.exports is the property object that we can export to other files
 
-module.exports.age = 25;
-
 module.exports.addNote = () => {
   console.log('Add note');
   return 'New note';
-}
-
-module.exports.add = (a, b) => {
-  return a + b;
 }
