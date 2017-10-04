9d9fea51109d914f4323f928bef53131b4e8729c
Patrick Miner <patrickminer318@gmail.com>
2017-10-03 21:00:50 -0700

    2nd Commit

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
-
-
-
+const _ = require('lodash');
 
 let user = os.userInfo();
 
