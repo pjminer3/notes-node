11b1a0be1c2b385b83ce9367d09d3d2799ab93a0
Patrick Miner <patrickminer318@gmail.com>
2017-10-05 16:47:30 -0700

    Adding and saving notes

--- notes.js
+++ notes.js
@@ -1,8 +1,33 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 // add notes
 var addNote = (title, body) => {
-  console.log(`Adding Note: "${title}" is the title, and "${body}" is the body`);
+  // creates default note array if needed
+  var notes = [];
+  // creates new note
+  var note = {
+    title,
+    body
+  }
+
+  // pulls existing notes if there are any
+  try {
+    var noteString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(noteString);
+  } catch (e) {
+    // Don't need any catch code here
+  }
+
+  // check for duplicate titles
+  var duplicateNotes = notes.filter((note) => note.title === title);
+  if (duplicateNotes.length === 0) {
+    // pushes new note into existing notes
+    notes.push(note);
+    // saves notes as a string in notes-data.json
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 }
 
 // gets all notes and lists them
