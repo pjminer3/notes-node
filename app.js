e68b6826922cb6426cf1c369a6cb0b0dd485c50e
Patrick Miner <patrickminer318@gmail.com>
2017-10-04 22:17:20 -0700

    Added Yargs module.  Added functions addNote(), getAll(), getNote(), removeNote() to notes.js. Can call the functions from app.js

--- app.js
+++ app.js
@@ -2,7 +2,27 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+// process.argv is automatically populated with all command line arguements
+
+const argv = yargs.argv; // command line arguments after being processed by yargs
+var command = process.argv[2]; // standard command line arguments
+console.log(`Command: ${command}`);
+console.log(`Yargs ARGV: `, argv);
+
+if ( command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+} else {
+  console.log('Command not recognized');
+}
 
 
--- notes.js
+++ notes.js
@@ -1,8 +1,38 @@
 console.log('Starting notes.js');
 
-//modue.exports is the property object that we can export to other files
-
-module.exports.addNote = () => {
-  console.log('Add note');
-  return 'New note';
+// add notes
+var addNote = (title, body) => {
+  console.log(`Adding Note: "${title}" is the title, and "${body}" is the body`);
 }
+
+// gets all notes and lists them
+var getAll = () => {
+  console.log('Getting all notes.');
+}
+
+// gets the note for people to read it
+var getNote = (title) => {
+  console.log(`Gets note ${title} for you to read`);
+}
+
+// removes note
+var removeNote = (title) => {
+  console.log(`Removes note ${title}`);
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+}
+
+
+
+
+// //modue.exports is the property object that we can export to other files
+
+// module.exports.addNote = () => {
+//   console.log('Add note');
+//   return 'New note';
+// }
